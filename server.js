a276165ae32ec2142e3ef1ebabcaffed5fa1157e
slemanSafiah <sleman-safiah@hotmail.com>
2020-08-20 04:22:23 +0300

    make it better

--- server.js
+++ server.js
@@ -1,72 +1,65 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Login = require('./routes/login');
-const Register = require('./routes/register');
-const Driver = require('./routes/driver');
-const User = require('./routes/user');
-const Trip = require('./routes/trip');
-const DriverM = require('./models/Driver');
-const UserM = require('./models/User');
-const TripM = require('./models/Trip');
-const Contact_us = require('./routes/contact_us');
-const cors = require('cors');
-const socketIo = require('socket.io');
-const http = require('http');
+const express = require("express");
+const mongoose = require("mongoose");
+const Login = require("./routes/login");
+const Register = require("./routes/register");
+const Driver = require("./routes/driver");
+const User = require("./routes/user");
+const Trip = require("./routes/trip");
+const DriverM = require("./models/Driver");
+const UserM = require("./models/User");
+const TripM = require("./models/Trip");
+const Contact_us = require("./routes/contact_us");
+const cors = require("cors");
+const socketIo = require("socket.io");
+const http = require("http");
 const app = express();
 
 app.use(cors());
 
-require('dotenv/config');
+require("dotenv/config");
 
 app.use(express.json());
 
 const server = http.createServer(app);
-
+server.listen(4001);
 const io = socketIo(server);
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-  socket.on('trip', (data) => {
-
+  socket.on("trip", (data) => {
     DriverM.findOne({ number: data.driver_number })
       .then((savesDriver) => {
-        if (!savesDriver)
-          return res.status(422).json({ error: "invalid Driver" });
-        else {
-          UserM.findOne({ number: data.user_number })
-            .then((savesUser) => {
-              if (!savesUser)
-                return res.status(422).json({ error: "invalid User" });
-              else {
-                const trip = new TripM({
-                  driver_number: data.driver_number,
-                  user_number: data.user_number,
-                  source_lat: data.source_lat,
-                  source_long: data.source_long,
-                  dest_lat: data.dest_lat,
-                  dest_long: data.dest_long,
-                  date: data.date
-                });
-                try {
-                  const savedTrip = trip.save()
-                  res.json({ sucess: 1 })
-                } catch (error) {
-                  res.json({ sucess: 0, message: error });
-                }
+        if (savesDriver) {
+          UserM.findOne({ number: data.user_number }).then((savesUser) => {
+            if (savesUser) {
+              console.log(data);
+              const trip = new TripM({
+                driver_number: data.driver_number,
+                user_number: data.user_number,
+                source_lat: data.source_lat,
+                source_long: data.source_long,
+                dest_lat: data.dest_lat,
+                dest_long: data.dest_long,
+              });
+              try {
+                const savedTrip = trip.save();
+                //res.json({ sucess: 1 });
+              } catch (error) {
+                //res.json({ sucess: 0, message: error });
               }
-            })
+            }
+          });
         }
       })
       .catch((err) => console.log(err));
-    socket.emit('notification', 'abc');
-  })
+    socket.emit("notification", "abc");
+  });
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
-
 
 mongoose.connect(
   process.env.DB_CONNECTION,
@@ -74,19 +67,19 @@ mongoose.connect(
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
-  () => console.log('connected to DB')
+  () => console.log("connected to DB")
 );
 
 mongoose.connection.on("error", () => {
   console.log("error from server");
 });
 
-app.use('/login', Login);
-app.use('/register', Register);
-app.use('/driver', Driver);
-app.use('/user', User);
-app.use('/contact_us', Contact_us);
-app.use('/trip', Trip);
+app.use("/login", Login);
+app.use("/register", Register);
+app.use("/driver", Driver);
+app.use("/user", User);
+app.use("/contact_us", Contact_us);
+app.use("/trip", Trip);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("running on port 5000"));
