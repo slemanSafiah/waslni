8f4a1d904db14832f2d1c5a43d176b23a3414488
slemanSafiah <sleman-safiah@hotmail.com>
2020-09-18 23:04:39 +0300

    update

--- server.js
+++ server.js
@@ -35,16 +35,18 @@ io.on("connection", (socket) => {
   console.log("a user connected");
 
   socket.on("trip", (data) => {
+    console.log(data);
     DriverM.findOne({
       number: data.driver_number
     })
       .then((savesDriver) => {
         if (savesDriver) {
+          console.log("find user");
           UserM.findOne({
             number: data.user_number
           }).then((savesUser) => {
             if (savesUser) {
-	console.og(savedUser);
+              console.log('123');
               const trip = new TripM({
                 driver_number: data.driver_number,
                 user_number: data.user_number,
@@ -68,6 +70,7 @@ io.on("connection", (socket) => {
                   a = tr.from;
                   b = tr.to;
                 }).then(() => {
+                  console.log(d_id, socket.id);
                   io.to(d_id).emit('notification', {
                     client: data.user_number,
                     source_lat: data.source_lat,
@@ -93,7 +96,7 @@ io.on("connection", (socket) => {
   })
 
   socket.on('message', (data) => {
-    console.log(data);
+    console.log("11111111111111111111111111111111", data);
     DriverM.findOne({
       number: data.driver
     })
@@ -108,21 +111,25 @@ io.on("connection", (socket) => {
                   driver: data.driver,
                   client: data.client,
                   message: data.message,
+                  is_driver: data.isdriver,
                   date: Date.now()
                 });
                 try {
-                  console.log(chat);
+                  console.log(data);
                   const savedChat = chat.save();
-                  savedChat.then((saved) => console.log(saved));
-                  console.log(data.isdriver)
-                  if (data.isdriver) {
-                    let u_id = users.get(data.client);
-                    socket.to(u_id).emit('message', data.message);
+                  savedChat.then((saved) => {
+                    if (data.isdriver) {
+                      let u_id = users.get(data.client);
+                      console.log(data, u_id);
 
-                  } else {
-                    let d_id = users.get(data.driver);
-                    socket.to(d_id).emit('message', data.message);
-                  }
+                      socket.to(u_id).emit('message', saved);
+
+                    } else {
+                      let d_id = users.get(data.driver);
+                      console.log('كم حرف', savedChat);
+                      socket.to(d_id).emit('message', saved);
+                    }
+                  });
                 } catch (error) {
                   console.log(error);
                 }
